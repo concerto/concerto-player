5758de6a2c7235b3c3803df5f2f433485cc60bdc
Andrew Armenia <andrew@asquaredlabs.com>
2014-05-01 01:29:17 -0400

    screen rotation and MAC detection via command line options
    screen rotation runs xrandr -o ${ROTATE} if a boot command line option
    concerto.rotate=${ROTATE} is passed in. ${ROTATE} can be normal,
    left, right, or inverted. See xrandr man page.
    
    MAC detection enables the Concerto 2 player to be used in a mode
    compatible with Concerto v1. If the boot command line contains the
    string "concerto.macdetect", a ?mac=xx:xx:xx:xx:xx:xx parameter
    will be appended to the end of the concerto.url.

--- chroot_tasks.sh
+++ chroot_tasks.sh
@@ -67,6 +67,17 @@ if [ -z $URL ]; then
 	URL=http://localhost:4567/screen
 fi
 
+ROTATE=`cat /proc/cmdline | perl -ne 'print "$1\n" if /concerto.rotate=(\S+)/'`
+if [ -n $ROTATE ]; then
+	xrandr -o $ROTATE
+fi
+
+MAC_DETECT=`cat /proc/cmdline | perl -ne 'print "1\n" if /concerto.mac_detect/'`
+if [ -n $MAC_DETECT ]; then
+	MAC=`/sbin/ifconfig eth0 | perl -ne 'print "$1\n" if /(([0-9a-f]{2}:){5}[0-9a-f]{2})/'`
+	URL=${URL}?mac=$MAC
+fi
+
 # start window manager
 blackbox &
 
